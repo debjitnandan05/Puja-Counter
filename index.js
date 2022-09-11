4e1e33a591ba2975cb81b27d085faf17590381de
DEBJIT NANDAN <debjitnandan05@gmail.com>
2022-09-11 11:33:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const d=document.getElementById("days");
+let h=document.getElementById('hours');
+let m=document.getElementById('minutes');
+let s=document.getElementById('seconds');
+
+
+let durgapuja='1 Oct 2022';
+
+function counter(){
+    let durgaPujaDate=new Date(durgapuja);
+    let currentDate=new Date();
+
+
+    let tSec= (durgaPujaDate-currentDate)/1000;
+    let days= Math.floor(tSec/3600/24);
+    let hour= Math.floor(tSec/3600)%24;
+    let min =((Math.floor(tSec/60))%60);
+    let sec = Math.floor(tSec)%60;
+    //console.log(days,hour,min,sec);
+
+d.innerHTML= days;
+h.innerHTML=timeFormat(hour);
+m.innerHTML=timeFormat(min);
+s.innerHTML=timeFormat(sec);
+
+}
+
+function timeFormat(time){
+    return (time < 10 ? '0' : '')+time;
+}
+
+counter();
+setInterval(counter,1000);
